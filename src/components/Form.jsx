55d8dc4a09260e81089c63bf29fd6b3c0ee645c7
MclPio <113801014+MclPio@users.noreply.github.com>
2024-12-17 17:37:00 -0500

    Add education form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,5 @@
 import { useState, useRef } from "react";
+import EducationForm from "./EducationForm";
 
 export default function Form({ onUpdateResume }) {
   const [contactInput, setContactInput] = useState({
@@ -30,34 +31,50 @@ export default function Form({ onUpdateResume }) {
   return (
     <>
       <dialog ref={dialogRef}>
-        <form className="contact-form">
-          <label htmlFor="name">name</label>
-          <input
-            type="text"
-            id="name"
-            value={contactInput.contactName}
-            onChange={(e) =>
-              setContactInput({ ...contactInput, contactName: e.target.value })
-            }
-          />
-          <label htmlFor="email">email</label>
-          <input
-            type="email"
-            id="email"
-            value={contactInput.contactEmail}
-            onChange={(e) =>
-              setContactInput({ ...contactInput, contactEmail: e.target.value })
-            }
-          />
-          <label htmlFor="phone">phone</label>
-          <input
-            type="tel"
-            id="phone"
-            value={contactInput.contactPhone}
-            onChange={(e) =>
-              setContactInput({ ...contactInput, contactPhone: e.target.value })
-            }
-          />
+        <form>
+          <fieldset className="contact-info">
+            <legend>Contact Info</legend>
+            <label htmlFor="name">name</label>
+            <input
+              type="text"
+              id="name"
+              value={contactInput.contactName}
+              onChange={(e) =>
+                setContactInput({
+                  ...contactInput,
+                  contactName: e.target.value,
+                })
+              }
+            />
+            <label htmlFor="email">email</label>
+            <input
+              type="email"
+              id="email"
+              value={contactInput.contactEmail}
+              onChange={(e) =>
+                setContactInput({
+                  ...contactInput,
+                  contactEmail: e.target.value,
+                })
+              }
+            />
+            <label htmlFor="phone">phone</label>
+            <input
+              type="tel"
+              id="phone"
+              value={contactInput.contactPhone}
+              onChange={(e) =>
+                setContactInput({
+                  ...contactInput,
+                  contactPhone: e.target.value,
+                })
+              }
+            />
+          </fieldset>
+          <EducationForm onUpdateResume></EducationForm>
+          <fieldset className="experience-info">
+            <legend>Experience</legend>
+          </fieldset>
           <button
             onClick={(e) => {
               handleSubmit(e);
