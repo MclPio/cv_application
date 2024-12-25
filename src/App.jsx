efea1382d0bf3ea799745957e88da01e9602302c
MclPio <113801014+MclPio@users.noreply.github.com>
2024-12-25 18:52:04 -0500

    Rewrite static page

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,103 @@
 import { useState } from "react";
-import Contact from "./components/Contact";
 import "./styles/app.css";
-import Education from "./components/Education";
-import Experience from "./components/Experience";
-import Form from "./components/Form";
 
-export default function App() {
-  const [name, setName] = useState("John Doe");
-  const [email, setEmail] = useState("johndoe@world.co");
-  const [phone, setPhone] = useState("123-123-1234");
-
-  const [school, setSchool] = useState("University of Toronto");
-  const [titleOfStudy, setTitleOfStudy] = useState("Finance");
-  const [dateOfStudy, setDateOfStudy] = useState("2015");
-
-  const [companyName, setCompanyName] = useState("AMD");
-  const [positionTitle, setPositionTitle] = useState("Hardware Engineer");
-  const [description, setDescription] = useState(
-    "Manged development of future tech chips and what not..."
-  );
-  const [startDate, setStartDate] = useState("2020");
-  const [endDate, setEndDate] = useState("present");
-
+function PersonalInfo() {
   return (
     <>
-      <div className="container">
-        <div className="resume">
-          <Contact name={name} email={email} phone={phone} />
-          <Education
-            school={school}
-            titleOfStudy={titleOfStudy}
-            dateOfStudy={dateOfStudy}
-          ></Education>
-          <Experience
-            companyName={companyName}
-            positionTitle={positionTitle}
-            description={description}
-            startDate={startDate}
-            endDate={endDate}
-          ></Experience>
+      <div className="personal-info">
+        <h1>name</h1>
+        <div className="personal-info-details">
+          <div>email</div>
+          <div>phone number</div>
+          <div>location</div>
+          <div>url</div>
         </div>
-        <Form
-          onUpdateResume={(name, email, phone) => {
-            setName(name);
-            setEmail(email);
-            setPhone(phone);
-          }}
-        ></Form>
       </div>
     </>
   );
 }
 
-// edit button should display on hover over resume section
-// FORM VIEW would be hidden until a user clicks a button to edit
+function School() {
+  return (
+    <>
+      <div className="school">
+        <div className="school-left">
+          <div>
+            <b>school name</b>
+          </div>
+          <div>title of study</div>
+        </div>
+        <div>date of study</div>
+      </div>
+    </>
+  );
+}
+
+function Education() {
+  return (
+    <>
+      <h2>Education</h2>
+      <div className="education">
+        <School />
+      </div>
+    </>
+  );
+}
+
+function ExperienceInfo() {
+  return (
+    <>
+      <div className="experience-info">
+        <div className="experience-info-details">
+          <div className="experience-info-left">
+            <div>Company Name</div>
+            <div>Position Title</div>
+          </div>
+          <div>Date</div>
+        </div>
+        <div>
+          Job Responsibilities
+          <ul>
+            <li>point 1</li>
+            <li>point 2</li>
+            <li>point 3</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function Experience() {
+  // company name, position title, main responsibilities of your jobs,
+  // date from and until when you worked for that company
+
+  return (
+    <>
+      <h2>Experience</h2>
+      <div className="experience">
+        <ExperienceInfo />
+      </div>
+    </>
+  );
+}
+
+function Resume() {
+  return (
+    <div className="resume">
+      <PersonalInfo />
+      <Education />
+      <Experience />
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <>
+      <div className="container">
+        <Resume />
+      </div>
+    </>
+  );
+}
